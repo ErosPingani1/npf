71f6bd620dc752645f01c5dd8e3388c72ded6f93
Eros Pingani <e.pingani3@gmail.com>
2021-10-31 16:07:06 +0100

    [DEV] - First commit (project creation and Logo component)

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Router from 'svelte-spa-router';
+	import Index from './routes/Index.svelte'
+</script>
+
+<Router routes={{
+	'/': Index
+}} />
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,10 @@
+<script>
+    const logo = 'images/npf.png';
+</script>
+
+<div class="logo">
+    <img src={logo} alt="title-logo" 
+        class="w-60 m-auto mt-12
+        md:w-80 
+        lg:w-96 lg:absolute lg:mt-52" />
+</div>
